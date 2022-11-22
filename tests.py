63214078dee31eb09ab84eab14771359e446b67d
minusmagis <33642118+minusmagis@users.noreply.github.com>
2022-11-22 13:38:50 +0300

    small functions added

--- tests.py
+++ tests.py
@@ -9,18 +9,18 @@ max = 300
 min = 0.05
 point_number = 200
 half_point_number = int(point_number/2)
-
-scale_factor_top = (max-mid_value)**(2/point_number)
-scale_factor_bot = (mid_value-min)**(2/point_number)
+multiplier = 100000
+scale_factor_top = (max-mid_value)**(2/(multiplier*point_number))
+scale_factor_bot = (mid_value-min)**(2/(multiplier*point_number))
 
 
 for i in range(half_point_number):
-  resistance_list_top.append(mid_value-1+scale_factor_top**i)
+  resistance_list_top.append(mid_value+scale_factor_top**(i*multiplier))
 
 resistance_list_top.pop(0)
 
 for i in range(half_point_number):
-  resistance_list_bot.append(mid_value+1-scale_factor_bot**i)
+  resistance_list_bot.append(mid_value-scale_factor_bot**(i*multiplier))
 
 resistance_list_bot.append(min)
 resistance_list_bot.reverse()
@@ -30,4 +30,4 @@ print(resistance_list_bot)
 
 resistance_list = resistance_list_bot+resistance_list_top
 
-print(resistance_list)
+# print(resistance_list)
